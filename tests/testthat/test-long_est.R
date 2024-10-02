172339d4d61a678cc8eb8d75780235d745a58575
Howard Baik <howardhowiebaek@gmail.com>
2024-10-02 10:14:07 -0400

    Start testing `long_est()`

--- tests/testthat/test-long_est.R
+++ tests/testthat/test-long_est.R
@@ -1,24 +1,43 @@
-# Setup parameters---
+test_that("The `standard_lme` method in long_est() works", {
+  # Setup parameters
+  outcome_var <- "Y"
+  time_var <- "time"
+  id_var <- "id"
+  VPM_variables <- c("Z", "X")
+  LM_fixedEffect_withoutTime_variables <- c("Z", "X")
+  LM_fixedEffect_withTime_variables <- c(c("Z", "X"), time_var)
+  LM_randomEffect_variables <- "Z"
 
-## Variables in the models ###
-# dataset
-outcome_var = "Y"
-time_var = "time"
-id_var = "id"
-VPM_variables = c("Z","X") # in the VP model
-LM_fixedEffect_withoutTime_variables = c("Z","X") # in the LP model
-LM_fixedEffect_withTime_variables = c(c("Z","X"),time_var) # in the LP model
-LM_randomEffect_variables = "Z" # in the LP model
-# Method = "LME" # All methods: LME, VA-LME, JMVL-LY, JMVL-Liang, JMVL-G, IIRR, imputation_LME
+  # Read in data
+  long_data <- read.csv(testthat::test_path("test-data", "train_data.csv"))
+  colnames(long_data)[which(colnames(long_data) == id_var)] <- "id"
+  colnames(long_data)[which(colnames(long_data) == outcome_var)] <- "Y"
+  colnames(long_data)[which(colnames(long_data) == time_var)] <- "time"
 
-# All methods: LME, VA-LME, JMVL-LY, JMVL-Liang, JMVL-G, IIRR, imputation_LME
-long_data = train_data
-colnames(long_data)[which(colnames(long_data)==id_var)] = "id"
-colnames(long_data)[which(colnames(long_data)==outcome_var)] = "Y"
-colnames(long_data)[which(colnames(long_data)==time_var)] = "time"
+  # Fit model
+  fit_standardLME <- long_est(long_data = long_data, method = "standard_LME",
+                              LM_fixedEffect_withTime_variables = LM_fixedEffect_withTime_variables,
+                              LM_randomEffect_variables = LM_randomEffect_variables)
+  # Extract coefficients
+  beta_hat <- unname(fit_standardLME$beta_hat)
+  beta_sd <- unname(fit_standardLME$beta_sd)
+
+  # Check beta_hat intercept
+  expect_equal(round(beta_hat[1], 3), -2.185)
+  # Check beta_hat Z
+  expect_equal(round(beta_hat[2], 3), -0.473)
+  # Check beta_hat X
+  expect_equal(round(beta_hat[3], 3), 0.591)
+  # Check beta_hat time
+  expect_equal(round(beta_hat[4], 3), 0.100)
 
 
-
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+  # Check beta_sd intercept
+  expect_equal(round(beta_sd[1], 3), 0.079)
+  # Check beta_sd Z
+  expect_equal(round(beta_sd[2], 3), 0.137)
+  # Check beta_sd X
+  expect_equal(round(beta_sd[3], 3), 0.066)
+  # Check beta_sd time
+  expect_equal(round(beta_sd[4], 3), 0.001)
 })
